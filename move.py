a05ddf98a37032f8d0f7f633b61b18366158c51c
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2023-03-20 16:51:14 +0100

    FIX search_account_kind.
    Task #048598

--- move.py
+++ move.py
@@ -81,7 +81,7 @@ class Line(metaclass=PoolMeta):
     @classmethod
     def search_account_kind(cls, name, clause):
         value = clause[2]
-        return [('account.type.%s'%value,) + tuple(clause[1], True)]
+        return [('account.type.%s'%value, *clause[1], True)]
 
     @fields.depends('account')
     def on_change_account(self):
